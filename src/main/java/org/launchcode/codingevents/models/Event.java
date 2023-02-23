10b73ade534b781569b9ca1651f3c239400d9660
Tuma Ussiri <tussiri@gmail.com>
2023-02-23 11:25:55 -0600

    finished first part of the code along

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,18 +1,42 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
-    private String name;
-    private String description;
-    private int id;
-    private static int nextId=1;
 
-    public Event(String name, String description) {
+    @NotBlank(message="Name is required")
+    @Size(min = 3, max = 20, message = "Name must be between 3 and 50 characters")
+    private String name;
+
+    @Size(max = 500, message = "Description exceeds the maximum length")
+    private String description;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email address. Try again")
+    private String contactEmail;
+
+    private int id;
+    private static int nextId = 1;
+
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
+        this.contactEmail = contactEmail;
         this.description = description;
         this.id = nextId;
         nextId++;
+    }
+
+    public Event (){}
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public int getId() {
