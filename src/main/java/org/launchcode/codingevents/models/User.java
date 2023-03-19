8ee602a0dfdce520db39964afa137f27d916b478
Tumaini Ussiri <40671546+tussiri@users.noreply.github.com>
2023-03-18 22:40:08 -0400

    finished Authentication

---
+++ src/main/java/org/launchcode/codingevents/models/User.java
@@ -0,0 +1,30 @@
+package org.launchcode.codingevents.models;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User extends AbstractEntity{
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    public User(String username, String password) {
+        this.username=username;
+        this.pwHash=encoder.encode(password);
+    }
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password,pwHash);
+    }
+}
